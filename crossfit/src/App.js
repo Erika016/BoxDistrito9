152c2936b85fc8f8968860a14cfd58f0032254dd
erika <ericadominguezbarrientos@gmail.com>
2022-06-17 00:46:09 +0200

    primer commit, he creado el home de mi pagina con varios componentes

---
+++ crossfit/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import {Card} from "./components/tarjetas";
+
+
+function App() {
+  return (
+    <div className="App">
+    <SectionBoxeo></SectionBoxeo>
+    </div>
+  );
+}
+
+export default App;
---
+++ crossfit/src/components/tarjetas/SectionBoxeo.js
@@ -0,0 +1,6 @@
+import { useParams } from "react-router-dom";
+// Nueva ventana donde esta la explicacion de todos los apartados de BOXEO
+export const SectionBoxeo = () => {
+  const params = useParams;
+  return <h1>Boxeo</h1>;
+};
